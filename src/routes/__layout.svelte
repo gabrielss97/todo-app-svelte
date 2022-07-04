e6daf8fc11cde314f869fb7f006e0263de9ace58
gabrielss97 <gabriel.ssalvador97@gmail.com>
2022-07-03 23:28:41 -0300

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+    <title>Todo App | Svelte</title>
+</svelte:head>
+
+<div class="container mx-auto my-6 max-w-lg text-center">
+	<slot />
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    import TodoForm from '../components/TodoForm.svelte'
+</script>
+
+<main>
+    <h1 class="text-2xl text-center text-gray-800 md:text-3xl font-bold">
+        My Todos
+    </h1>
+    <TodoForm/>
+</main>
